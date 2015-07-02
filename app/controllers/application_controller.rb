7b2b5cf7d2dadc0a61582cf37bda62d693229253
TsuyoshiUshio <ushio@simplearchitect.com>
2015-07-02 11:18:30 +0900

    Change the routes after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_out_path_for(resource)
+    "/users/sign_in"
+  end
 end
