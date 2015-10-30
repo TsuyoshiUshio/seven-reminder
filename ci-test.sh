9d4df163d2d20aaa5bea375f81dd1c223c8c5696
TsuyoshiUshio <ushio@simplearchitect.com>
2015-10-31 03:35:28 +0900

    libxml2 problem solution spike

--- ci-test.sh
+++ ci-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+bundle config build.nokogiri --use-system-libraries
 RAILS_ENV=test;bundle install
 RAILS_ENV=test;bundle exec rake db:create
 RAILS_ENV=test;bundle exec rake db:migrate
