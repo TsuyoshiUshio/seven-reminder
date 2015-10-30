584b7aaf7048eff778f56dd6d2bd320d7bba7be7
TsuyoshiUshio <ushio@simplearchitect.com>
2015-10-31 02:11:39 +0900

    add rm

--- ci-test.sh
+++ ci-test.sh
@@ -3,4 +3,5 @@
 RAILS_ENV=test;bundle install
 RAILS_ENV=test;bundle exec rake db:create
 RAILS_ENV=test;bundle exec rake db:migrate
+rm -rf spec/reports
 RAILS_ENV=test;bundle exec rspec -f JUnit -o spec/reports/results.xml
