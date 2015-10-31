e6caaef2211744f46c99481435654cd7726f5879
TsuyoshiUshio <ushio@simplearchitect.com>
2015-10-31 11:35:45 +0900

    Rspec only execution

--- ci-test.sh
+++ ci-test.sh
@@ -4,5 +4,5 @@ RAILS_ENV=test;bundle install
 RAILS_ENV=test;bundle exec rake db:create
 RAILS_ENV=test;bundle exec rake db:migrate
 rm -rf spec/reports
-RAILS_ENV=test;bundle exec rake
+ RAILS_ENV=test;bundle exec rspec --format RspecJunitFormatter --out spec/reports/rspecs.xml
 
